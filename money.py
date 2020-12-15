8281e3dcb07feec0588469c84780aec486e1e179
Tracy Falba <tfalba@mac.com>
2020-12-15 07:23:21 -0500

    all passing tests

--- money.py
+++ money.py
@@ -6,34 +6,44 @@ class DifferentCurrencyError(Exception):
 
 
 class Currency:
-    """
-    Represents a currency. Does not contain any exchange rate info.
-    """
+    # """
+    # Represents a currency. Does not contain any exchange rate info.
+    # """
 
     def __init__(self, name, code, symbol=None, digits=2):
-        """
-        Parameters:
-        - name -- the English name of the currency
-        - code -- the ISO 4217 three-letter code for the currency
-        - symbol - optional symbol used to designate currency
-        - digits -- number of significant digits used
-        """
-        pass
+        self.name = name
+        self.code = code
+        self.symbol = symbol
+        self.digits = digits
+        
+        # """
+        # Parameters:
+        # - name -- the English name of the currency
+        # - code -- the ISO 4217 three-letter code for the currency
+        # - symbol - optional symbol used to designate currency
+        # - digits -- number of significant digits used
+        # """
+        #pass
 
     def __str__(self):
+        if self.symbol == None:
+            return f'{self.code}'
+        else:
+            return f'{self.code} ({self.symbol})'
         """
         Should return the currency code, or code with symbol in parentheses.
         """
-        pass
+        #pass
 
     def __eq__(self, other):
         """
         All fields must be equal to for the objects to be equal.
         """
+        
         return (type(self) == type(other) and self.name == other.name and
                 self.code == other.code and self.symbol == other.symbol and
                 self.digits == other.digits)
-
+        
 
 class Money:
     """
@@ -46,15 +56,23 @@ class Money:
         - amount -- quantity of currency
         - currency -- type of currency
         """
-        pass
+        self.amount = amount
+        self.currency = currency
+        #pass
 
     def __str__(self):
         """
         Should use the currency symbol if available, else use the code.
         Use the currency digits to determine number of digits to show.
         """
-        pass
-
+        
+        x= "{:.{}f}"        #What is x???
+        formatted_amount = x.format(self.amount, self.currency.digits)
+        if self.currency.symbol:
+            return f'{self.currency.symbol}{formatted_amount}'
+        else:
+            return f'{self.currency.code} {formatted_amount}'
+        
     def __repr__(self):
         return f"<Money {str(self)}>"
 
@@ -67,27 +85,46 @@ class Money:
                 self.currency == other.currency)
 
     def add(self, other):
+
+        if self.currency == other.currency:
+            return Money(self.amount + other.amount, self.currency)
+        else:
+            raise DifferentCurrencyError
         """
         Add two money objects of the same currency. If they have different
         currencies, raise a DifferentCurrencyError.
         """
-        pass
+        #pass
 
     def sub(self, other):
+        if self.currency == other.currency:
+            return Money(self.amount - other.amount, self.currency)
+        else:
+            raise DifferentCurrencyError
         """
         Subtract two money objects of the same currency. If they have different
         currencies, raise a DifferentCurrencyError.
         """
-        pass
+        #pass
 
     def mul(self, multiplier):
+        return Money(self.amount * multiplier, self.currency)
+        
         """
         Multiply a money object by a number to get a new money object.
         """
-        pass
+        #pass
 
     def div(self, divisor):
+        return Money(self.amount / divisor, self.currency)
         """
         Divide a money object by a number to get a new money object.
         """
-        pass
+        #pass
+
+USD = Currency('Dollar', 'USD', '$', 2)
+print(USD)
+one_dollar = Money(1, USD)
+#one_dollar = Money(1, Currency('Dollar', 'USD', '$', 2))
+print(one_dollar)
+
